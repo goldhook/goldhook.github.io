921d07192f8201ec3dd489de12b7007771c3f07e
Will Scott <will@goldhook.co.uk>
2016-04-27 09:21:48 +0100

    First commit for GH Pages test

---
+++ inc/header.php
@@ -0,0 +1,26 @@
+<div id="container">
+		 <div id="header">
+		 			<div class="logo">
+					<h1>Brass Tack</h1>
+			
+					</div>
+					
+					<div class="navigation">
+							 <div class="mainnav">
+							 			<div class="navgrid" id="home"><h2><a accesskey="1" href="/">home <span class="chevron">&raquo;</span></a></h2>
+										<p>take me<br />back to base</p>
+							 			</div>
+							 			<div class="navgrid" id="about"><h2><a accesskey="2" href="#!about">about us <span class="chevron">&raquo;</span></a> </h2>
+										<p>who we are and <br />what we know</p>
+										</div>
+										<div class="navgrid" id="products"><h2><a accesskey="3" href="/services.php">services <span class="chevron">&raquo;</span></a></h2>
+										<p>what we do and <br />what you get</p>
+										</div>
+										<div class="navgrid" id="contact"><h2><a accesskey="4" href="/contact.php">contact <span class="chevron">&raquo;</span></a></h2>
+										<p>give us a call <br />or email us</p>
+										</div>
+							 </div>										
+					</div>
+			</div>
+
+			<div id="bodycontent">
